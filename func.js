6255ccc740b46e885074312e98ab15b28c7ae8dc
khaled-as <khaled_abdel_salam@hotmail.com>
2025-08-22 10:12:09 -0400

    Add files via upload

--- func.js
+++ func.js
@@ -1,10 +1,19 @@
 const fdk=require('@fnproject/fdk');
 
-fdk.handle(function(input){
-  let name = 'World';
-  if (input.name) {
-    name = input.name;
-  }
-  console.log('\nInside Node Hello World function')
-  return {'message': 'Hello ' + name}
+fdk.handle(function(input, ctx){
+	async function fetchData() {
+		try {
+			const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+			if (!response.ok) {
+			  throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			console.log(response);
+			const data = await response.json();
+			console.log(data);
+			return data;
+		} catch (error) {
+			console.error('Error fetching data:', error);
+		}
+	}
+  return {"response": fetchData()}
 })
